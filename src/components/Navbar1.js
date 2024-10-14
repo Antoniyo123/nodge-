f6f1fe561da29a2fbd9f622ee782549b31444ee9
Riswanton Ramudin <riswantonr2015@gmail.com>
2024-10-15 04:48:35 +0700

    adjust rotation hamburger

--- src/components/Navbar1.js
+++ src/components/Navbar1.js
@@ -18,57 +18,55 @@ const NavBar = () => {
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-    
   };
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
-      
       if (currentScrollY > lastScrollY) {
         setIsVisible(false);
       } else {
         setIsVisible(true);
       }
-
       setIsScrolled(currentScrollY > 50);
       setLastScrollY(currentScrollY);
     };
 
     window.addEventListener('scroll', handleScroll, { passive: true });
-
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, [lastScrollY]);
 
   return (
-      <nav className={`navbar ${isScrolled ? 'navbar-scrolled' : 'navbar-transparent'} ${isVisible ? 'navbar-visible' : 'navbar-hidden'}`}>
-        <div className="navbar-brand">
-          <img src={require('../img/hero/NODGE LOGO.png')} alt="NODGE+" />
-        </div>
+    <nav className={`navbar ${isScrolled ? 'navbar-scrolled' : 'navbar-transparent'} ${isVisible ? 'navbar-visible' : 'navbar-hidden'} ${isMenuOpen ? 'menu-open' : ''}`}>
+      <div className={`navbar-container ${isMenuOpen ? 'hidden' : ''}`}>
+        <a href="/" className="navbar-brand">
+          <img src={require('../img/hero/NODGE LOGO.png')} alt="Logo" />
+        </a>
         <button className="hamburger" onClick={toggleMenu}>
-          <img src={require('../img/hero/NODGE+.png')} alt="Menu" />
-        </button>
-        {isMenuOpen && (
-          <div className={`menu-overlay ${isMenuOpen ? 'menu-open' : ''}`}>
-            <button className="close-button" onClick={toggleMenu}>
-              &times;
-            </button>
-            <div className="grid-menu">
-              {categories.map((category, index) => (
-                <div key={category.name} className={`menu-item ${isMenuOpen ? 'menu-item-visible' : ''}`} style={{ transitionDelay: `${index * 0.1}s` }}>
-                  <img src={category.image} alt={category.name} />
-                  <div className="menu-item-overlay">
-                    <span className="menu-item-text">{category.name}</span>
-                  </div>
-                </div>
-              ))}
-            </div>
+          <div className="hamburger-icon-wrapper">
+            <img className="hamburger-icon" src={require('../img/hero/NODGE+.png')} alt="Menu" />
+            <img className="hamburger-icon-rotated" src={require('../img/hero/NODGE+.png')} alt="Menu Rotated" />
           </div>
-        )}
-      </nav>
-   
+        </button>
+      </div>
+      <div className={`menu-overlay ${isMenuOpen ? 'menu-open' : ''}`}>
+        <div className="menu-content">
+          <button className="close-button" onClick={toggleMenu}>×</button>
+          <div className="grid-menu">
+            {categories.map((category, index) => (
+              <div key={index} className="menu-item" style={{transitionDelay: `${index * 0.05}s`}}>
+                <img src={category.image} alt={category.name} />
+                <div className="menu-item-overlay">
+                  <span className="menu-item-text">{category.name}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
